0020e61faf19068bf1185a9dd32a5620990e87ca
kyungseopark <separk823@gmail.com>
2026-05-21 21:55:46 +0900

    fix: solar 모델 use_vision=False 수정

--- solar_test.py
+++ solar_test.py
@@ -51,7 +51,7 @@ MODELS = {
     ),
     "solar": lambda: (
         ChatOpenAI(model=VLLM_MODEL, base_url=VLLM_BASE_URL, api_key="dummy", temperature=0.0),
-        True,
+        False,
     ),
 }
 
